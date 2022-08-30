06d1c69424f4e813947ecd7a49618401e862dd1b
Ashley Davis <ashley@codecapers.com.au>
2022-08-31 09:33:14 +1000

    Simple gallery baseline.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,55 @@
+const gallery = require("./gallery.json");
+
+//
+// Uncomment this to log all gallery data:
+//
+// console.log(gallery);
+//
+
 export function App() {
-    return <h1>Hello computer!</h1>;
+
+    const rows = [];
+
+    let curRow = {
+        items: [], 
+    };
+    rows.push(curRow);
+
+    for (const item of gallery) {
+        curRow.items.push(item);
+    }
+
+    const galleryWidth = 600;
+
+    return (
+        <div
+            style={{
+                width: `${galleryWidth}px`,
+                border: "solid green 1px",
+            }}
+            >
+            {rows.map((row, rowIndex) => {
+                return (
+                    <div
+                        key={rowIndex}
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            height: `200px`,
+                        }}
+                        >
+                        {row.items.map(item => {
+                            return (
+                                <img 
+                                    key={item.urls.thumb}
+                                    src={item.urls.thumb} 
+                                    />
+                            );
+                        })}
+                    </div>
+                );
+            })}
+
+        </div>
+    );
 }
